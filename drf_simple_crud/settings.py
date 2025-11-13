002fb644ac53c1cd3c09ca1f61a1d05e2a16e7ed
porongusaurio <counterjosefoo@gmail.com>
2025-11-12 21:49:36 -0300

    Add collected static files

--- drf_simple_crud/settings.py
+++ drf_simple_crud/settings.py
@@ -22,12 +22,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 'dev-secret-key')
+SECRET_KEY = os.environ.get('SECRET_KEY', '!j3x7!qz2ag%-$f7h!5(1ga&zf0ao!b3hb9ejvx5-+4wzxrzd8')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'RENDER' not in os.environ
 
-ALLOWED_HOSTS = ['localhost', os.environ.get('RENDER_EXTERNAL_HOSTNAME', '')]
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    os.environ.get('RENDER_EXTERNAL_HOSTNAME', '')
+]
+
 
 
 # Application definition
