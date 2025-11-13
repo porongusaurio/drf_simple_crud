f6ece32a81bdb6282d0b99c2b67cad5c60e1484b
porongusaurio <counterjosefoo@gmail.com>
2025-11-13 03:54:53 -0300

    feat: Agregado redireccionamiento de la URL raíz a /api/

--- drf_simple_crud/urls.py
+++ drf_simple_crud/urls.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect # <--- 1. Importa redirect
+
+# 2. Función para redirigir
+def redirect_to_api(request):
+    return redirect('/api/') # Te manda a la URL de tu API
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('projects.urls')),
+    path('', redirect_to_api), # <--- 3. Redirección automática
 ]
